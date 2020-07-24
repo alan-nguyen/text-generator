2f71c29cf3a363454053ff16e0c00596b6b3fdfe
Alan Nguyen <tuan8101@gmail.com>
2020-07-23 22:38:52 -0600

    Add constructor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return <div className="App">Hello</div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      paras: 4,
+      html: true,
+      text: '',
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Hello
+        <p></p>
+      </div>
+    );
+  }
 }
 
 export default App;
